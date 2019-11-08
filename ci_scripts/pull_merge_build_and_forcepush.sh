1ac87850366d0a32795fd1dfc51bea8ab8ff6d3c
devdoomari <devtest1mari@gmail.com>
2019-11-08 13:38:59 +0900

    123123123

--- ci_scripts/pull_merge_build_and_forcepush.sh
+++ ci_scripts/pull_merge_build_and_forcepush.sh
@@ -6,20 +6,15 @@ then
 fi
 if [ "${REPO_BRANCH}" == "develop" ]
 then
-  git checkout master
+  DIST_PUSH_BRANCH="master"
 else
-  git checkout -b $'dist-'"$REPO_BRANCH" || git checkout $'dist-'"$REPO_BRANCH"
+  DIST_PUSH_BRANCH=$'dist-'"$REPO_BRANCH"
 fi
-git checkout master
+git checkout -b $DIST_PUSH_BRANCH || git checkout $DIST_PUSH_BRANCH
 mkdir -p ~/.git && git config user.email "devdoomari@gmail.com" && git config user.name "devdoomari.circleci"
 git merge -X theirs --no-edit $REPO_BRANCH
 npm run clean && npm run build
 git add -f lib
 git commit --allow-empty -m $'generated from:'"$COMMIT_HASH"$'\ntriggered by:'"$USERNAME"$'\n[ci skip]'
 git tag -a $'dist_'"$REPO_BRANCH"'_'"$BUILD_NUM" -m "."
-if [ "${REPO_BRANCH}" == "develop" ]
-then
-  git push --set-upstream origin master --force --follow-tags
-else
-  git push --set-upstream origin $'dist-'"$REPO_BRANCH" --force --follow-tags
-fi
+git push --set-upstream origin $DIST_PUSH_BRANCH --force --follow-tags
