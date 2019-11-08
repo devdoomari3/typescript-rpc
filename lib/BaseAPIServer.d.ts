468a3a11bb5e9b1b18a42f11388d5423aca89102
devdoomari.circleci <devdoomari@gmail.com>
2019-11-08 04:39:33 +0000

    generated from:1ac87850366d0a32795fd1dfc51bea8ab8ff6d3c triggered by:devdoomari3 [ci skip]

---
+++ lib/BaseAPIServer.d.ts
@@ -0,0 +1,13 @@
+import { Omit } from 'typelevel-ts';
+import { ReqRespAPIType, BaseResponseType, UnpackReqRespAPIType } from './types';
+export declare type CleanedResponseType<T extends BaseResponseType = BaseResponseType> = Omit<T, '___BaseResponseType'>;
+export declare abstract class BaseAPIServer {
+    handlers: {
+        [apiName: string]: (req: any) => Promise<CleanedResponseType>;
+    };
+    addAPI<APIType extends ReqRespAPIType<any, any, any>>(api: APIType, handler: (req: UnpackReqRespAPIType<APIType>['RequestType']) => Promise<CleanedResponseType<UnpackReqRespAPIType<APIType>['ResponseType']>>): void;
+    checkAPIAllImplemented(apis: {
+        [name: string]: ReqRespAPIType<any, any, string>;
+    }): void;
+    abstract cleanUp(): void;
+}
