0c41fd1d78bf9ef7a9c592fedcd5a0b31f5212d9
devdoomari.circleci <devdoomari@gmail.com>
2020-01-16 09:09:41 +0000

    generated from:6d577ab17493eef162eb6c7fcdd16d6aa702aa62 triggered by:devdoomari3 [ci skip]

---
+++ lib/BaseAPIClient.d.ts
@@ -0,0 +1,5 @@
+import { ReqRespAPIType, BaseRequestType, BaseResponseType, UnpackReqRespAPIType } from './types';
+export declare type APICall<RequestType extends BaseRequestType, ResponseType extends BaseResponseType, name extends string> = (request: Omit<RequestType, '___BaseRequestType'>) => Promise<ResponseType>;
+export declare abstract class BaseAPIClient {
+    abstract useAPI<APIType extends ReqRespAPIType<any, any, any>>(api: APIType): APICall<UnpackReqRespAPIType<APIType>['RequestType'], UnpackReqRespAPIType<APIType>['ResponseType'], UnpackReqRespAPIType<APIType>['name']>;
+}
